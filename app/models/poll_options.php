65de963bcf5215f691f74f5b8c331d17fe71ddfa
Pinja-Liina Jalkanen <plj@iki.fi>
2016-02-13 21:27:48 +0200

    Added inital models for poll options and votes.

---
+++ app/models/poll_options.php
@@ -0,0 +1,74 @@
+<?php
+
+  class PollOption extends BaseModel{
+		
+		//Attributes
+		// NOTE: in real life the password should be salted and hashed (and the
+		// string hidden from the UI), but as that isn't included in the course
+		// requirements, I won't implement it.
+		public $id, $polls_id, $name, $description;
+		
+		//Constructor
+		public function __construct($attributes){
+			parent::__construct($attributes);
+		}
+		
+		private static function tbl() {
+			return DatabaseConfig::PREFIX .'poll_options';
+		}
+		
+		public static function findByPollId($polls_id){
+			$q = DB::connection()->prepare('SELECT * FROM '. self::tbl() .' WHERE polls_id = :polls_id');
+			$q->execute(array('polls_id' => $polls_id));
+			$rows = $q->fetchAll();
+			
+			$polloptions = array();
+			foreach($rows as $r) {
+				$polloptions[] = new PollOption(array(
+						'id' => $r['id'],
+						'polls_id' => $r['polls_id'],
+						'name' => $r['name'],
+						'description' => $r['description']
+				));
+			}
+			
+			return $polloptions;
+		}
+		
+		public static function findByPK($id){
+			$q = DB::connection()->prepare('SELECT * FROM '. self::tbl() .' WHERE id = :id');
+			$q->execute(array('id' => $id));
+			$r = $q->fetch();
+			
+			if($r) {
+				$polloption = new PollOption(array(
+						'id' => $r['id'],
+						'polls_id' => $r['polls_id'],
+						'name' => $r['name'],
+						'description' => $r['description']
+				));
+			}
+			
+			return $polloption;
+			
+		}
+		
+		public function save(){
+			$q = DB::connection()->prepare('INSERT INTO '. self::tbl() .' (polls_id, name, description) VALUES (:polls_id, :name, :description) RETURNING id');
+			$q->execute(array($this->polls_id, $this->name, $this->description));
+			$row = $q->fetch();
+			if($row['id']){
+				$this->id = $row['id'];
+			}
+		}
+		
+		public function update(){
+			$q = DB::connection()->prepare('UPDATE '. self::tbl() .' SET (polls_id, name, description)=(:polls_id, :name, :description) WHERE id = :id');
+			$q->execute(array($this->polls_id, $this->name, $this->description, $this->id));
+		}
+		
+		public function delete(){
+			$q = DB::connection()->prepare('DELETE FROM '. self::tbl() .' WHERE id = :id');
+			$q->execute(array($this->id));
+		}
+	}
---
+++ app/models/votes.php
@@ -0,0 +1,92 @@
+<?php
+
+  class Vote extends BaseModel{
+		
+		//Attributes
+		// NOTE: in real life the password should be salted and hashed (and the
+		// string hidden from the UI), but as that isn't included in the course
+		// requirements, I won't implement it.
+		public $id, $polls_id, $poll_options_id, $time;
+		
+		//Constructor
+		public function __construct($attributes){
+			parent::__construct($attributes);
+		}
+		
+		private static function tbl() {
+			return DatabaseConfig::PREFIX .'votes';
+		}
+		
+		public static function findByPollId($polls_id){
+			$q = DB::connection()->prepare('SELECT * FROM '. self::tbl() .' WHERE polls_id = :polls_id');
+			$q->execute(array('polls_id' => $polls_id));
+			$rows = $q->fetchAll();
+			
+			$votes = array();
+			foreach($rows as $r) {
+				$votes[] = new Vote(array(
+						'id' => $r['id'],
+						'polls_id' => $r['polls_id'],
+						'poll_options_id' => $r['poll_options_id'],
+						'time' => $r['time']
+				));
+			}
+			
+			return $votes;
+		}
+		
+		public static function findByPollIdAndOptionId($polls_id, $poll_options_id){
+			$q = DB::connection()->prepare('SELECT * FROM '. self::tbl() .' WHERE polls_id = :polls_id AND poll_options_id = :poll_options_id');
+			$q->execute(array('polls_id' => $polls_id, 'poll_options_id' => $poll_options_id));
+			$rows = $q->fetchAll();
+			
+			$votes = array();
+			foreach($rows as $r) {
+				$votes[] = new Vote(array(
+						'id' => $r['id'],
+						'polls_id' => $r['polls_id'],
+						'poll_options_id' => $r['poll_options_id'],
+						'time' => $r['time']
+				));
+			}
+			
+			return $votes;
+		}
+		
+		public static function findByPK($id){
+			$q = DB::connection()->prepare('SELECT * FROM '. self::tbl() .' WHERE id = :id');
+			$q->execute(array('id' => $id));
+			$r = $q->fetch();
+			
+			if($r) {
+				$vote = new Vote(array(
+						'id' => $r['id'],
+						'polls_id' => $r['polls_id'],
+						'poll_options_id' => $r['poll_options_id'],
+						'time' => $r['time']
+				));
+			}
+			
+			return $vote;
+			
+		}
+		
+		public function save(){
+			$q = DB::connection()->prepare('INSERT INTO '. self::tbl() .' (polls_id, poll_options_id, time) VALUES (:polls_id, :poll_options_id, :time) RETURNING id');
+			$q->execute(array($this->polls_id, $this->poll_options_id, $this->time));
+			$row = $q->fetch();
+			if($row['id']){
+				$this->id = $row['id'];
+			}
+		}
+		
+		public function update(){
+			$q = DB::connection()->prepare('UPDATE '. self::tbl() .' SET (polls_id, poll_options_id, time)=(:polls_id, :poll_options_id, :time) WHERE id = :id');
+			$q->execute(array($this->polls_id, $this->poll_options_id, $this->time, $this->id));
+		}
+		
+		public function delete(){
+			$q = DB::connection()->prepare('DELETE FROM '. self::tbl() .' WHERE id = :id');
+			$q->execute(array($this->id));
+		}
+	}
