2de1e3150e211f815011a658c43ec9bddbc5ad49
Pinja-Liina Jalkanen <plj@iki.fi>
2016-02-14 10:31:45 +0200

    Removed accidential copy-paste duplicates from routes.

--- config/routes.php
+++ config/routes.php
@@ -32,26 +32,6 @@
 		UserController::delete($id);
 	});
 
-  $routes->get('/hiekkalaatikko', function() {
-    HelloWorldController::sandbox();
-  });
-	
-	$routes->get('/login', function() {
-		HelloWorldController::login();
-	});
-
-	$routes->get('/poll_list', function() {
-		HelloWorldController::poll_list();
-	});
-
-	$routes->get('/poll_show', function() {
-		HelloWorldController::poll_show();
-	});
-	
-	$routes->get('/poll_edit', function() {
-		HelloWorldController::poll_edit();
-	});
-
 	$routes->get('/poll', function() {
 		PollController::index();
 	});
