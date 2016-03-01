pinjaliina/aanestys

commit 5df64bd04d1d86c8d258c706c358b54779e976eb

Display poll results and statistics in poll/show.html.

app/models/poll.php
@@ -235,8 +235,9 @@
 					++$r['vote_count'];				
 					++$r['options'][$option->id]['vote_count'];				
 				}
+				$r['options'][$option->id]['percentage'] = round(($r['options'][$option->id]['vote_count']/$r['eligible_count'])*100);
 			}
-			$r['turnout'] = ($r['vote_count']/$r['eligible_count'])*100;
+			$r['turnout'] = round(($r['vote_count']/$r['eligible_count'])*100);
 			uasort($r['options'], "self::cmpVoteCount");
 			
 			return $r;

app/controllers/poll_controller.php
@@ -22,11 +22,11 @@
 			if(!$curruser->admin) {
 				Redirect::to('/user/'. $curruser->id, array('warning' => 'Pääsy kielletty ilman ylläpito-oikeutta!'));			
 			}
-			$thispoll = Poll::findByPK($id);
-			$results = $thispoll->getResults();
-			Kint::dump($results);
-			$poll = array('poll' => $thispoll, 'polloptions' => PollOption::findByPollId($id));
-			View::make('poll/show.html', $poll);
+			$poll = Poll::findByPK($id);
+			$polloptions = PollOption::findByPollId($id);
+			$results = $poll->getResults();
+			$params = array('poll' => $poll, 'polloptions' => $polloptions, 'results' => $results);
+			View::make('poll/show.html', $params);
 		}
 
 		public static function create(){

app/views/poll/show.html
@@ -41,6 +41,44 @@
 		<a class="btn btn-default" href="{{base_path}}/poll/{{poll.id}}/delete">Poista äänestys</a>
 	</div>
 	
-	<h1>Äänestyksen tulokset</h1>
+	<h1>Äänestyksen tilastot ja tulokset</h1>
+	
+	<h2>Tilastot</h2>
+	
+	<div class="box_basic" id="poll_statistics">
+		<table>
+			<tr>
+				<th>Äänioikeutettuja yhteensä</th>
+				<td>{{results.eligible_count}}</td>
+			</tr>
+			<tr>
+				<th>Annettuja ääniä yhteensä</th>
+				<td>{{results.vote_count}}</td>
+			</tr>
+			<tr>
+				<th>Äänestysprosentti</th>
+				<td>{{results.turnout}} %</td>
+			</tr>
+		</table>
+	</div>
 
+	<h2>Tulokset</h2>
+	
+	<div class="box_basic" id="poll_results">
+		<table>
+			<tr>
+				<th>Äänestyksen vaihtoehto</th>
+				<th>Annettuja ääniä</th>
+				<th>Osuus annetuista äänistä</th>
+			</tr>
+			{% for option in results.options %}
+				<tr>
+					<td>{{option.name}}</td>
+					<td>{{option.vote_count}}</td>
+					<td>{{option.percentage}} %</td>
+				</tr>
+			{% endfor %}
+		</table>
+	</div>
+	
 {% endblock %}

commit 909b77d268044192427c049aa1605c8dd56864c9

Fixed a division by zero bug caused by 5df64bd04d1d86c8d258c706c358b54779e976eb in poll/show.html when there are no eligible users in a poll.

app/models/poll.php
@@ -235,9 +235,9 @@
 					++$r['vote_count'];				
 					++$r['options'][$option->id]['vote_count'];				
 				}
-				$r['options'][$option->id]['percentage'] = round(($r['options'][$option->id]['vote_count']/$r['eligible_count'])*100);
+				$r['options'][$option->id]['percentage'] = $r['eligible_count'] > 0 ? round(($r['options'][$option->id]['vote_count']/$r['eligible_count'])*100) : 0;
 			}
-			$r['turnout'] = round(($r['vote_count']/$r['eligible_count'])*100);
+			$r['turnout'] = $r['eligible_count'] > 0 ? round(($r['vote_count']/$r['eligible_count'])*100) : 0;
 			uasort($r['options'], "self::cmpVoteCount");
 			
 			return $r;